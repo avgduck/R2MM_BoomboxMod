74560684f8bbade1d022654ef0dadced8fd1dbf1
avgduck <avgduck@gmail.com>
2025-08-05 02:01:33 -0400

    Create package files for R2MM release

--- PluginDetails.cs
+++ PluginDetails.cs
@@ -4,14 +4,18 @@ namespace R2MM_BoomboxMod;
 
 public static class PluginDetails
 {
+    
     public const string PLUGIN_GUID = "avgduck.plugins.llb.r2mm-boomboxmod";
-    public const string PLUGIN_NAME = "R2MM-BoomboxMod";
+    public const string PLUGIN_NAME = "R2MM_BoomboxMod";
     public const string PLUGIN_VERSION = "1.0.0";
     
     public const string DEPENDENCY_LLBMODDINGLIB = "fr.glomzubuk.plugins.llb.llbml";
     public const string DEPENDENCY_MODMENU = "no.mrgentle.plugins.llb.modmenu";
 
     public static readonly List<string> MODMENU_TEXT = [
-        
+        "This mod allows the user to toggle both the music text and timer text of the in-game boombox HUD"
     ];
+    
+    
+    
 }
---
+++ avg_duck-R2MM_BoomboxMod/README.md
@@ -0,0 +1,7 @@
+## BoomboxMod for R2ModMan
+Allows the user to individually hide the music text and timer text on the in-game boombox HUD.\
+Adjustable using ModMenu or the config file.
+Compatible with Glomzubuk's StageAsMusicName mod.
+
+Idea from Daioutzu's LLBModManager version of the mod.
+Improved to have config options for quick toggling in ModMenu and call the hiding logic only once each time a screen transition happens instead of once every frame.
