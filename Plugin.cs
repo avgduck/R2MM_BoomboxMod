8aaf5f52a27f6d800117db8c2b4d18660f1be5c5
avgduck <avgduck@gmail.com>
2025-08-04 23:38:13 -0400

    Create boilerplate plugin code

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using LLBML.Utils;
+
+namespace R2MM_BoomboxMod;
+
+[BepInPlugin(PluginDetails.PLUGIN_GUID, PluginDetails.PLUGIN_NAME, PluginDetails.PLUGIN_VERSION)]
+[BepInProcess("LLBlaze.exe")]
+[BepInDependency(PluginDetails.DEPENDENCY_LLBMODDINGLIB)]
+[BepInDependency(PluginDetails.DEPENDENCY_MODMENU, BepInDependency.DependencyFlags.SoftDependency)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static Plugin Instance { get; private set; }
+    internal static ManualLogSource GlobalLogSource { get; private set; }
+        
+    private void Awake()
+    {
+        Instance = this;
+        
+        GlobalLogSource = base.Logger;
+        GlobalLogSource.LogInfo($"Plugin {PluginDetails.PLUGIN_NAME} is loaded!");
+    }
+
+    private void Start()
+    {
+        ModDependenciesUtils.RegisterToModMenu(base.Info, PluginDetails.MODMENU_TEXT);
+    }
+}
---
+++ PluginDetails.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace R2MM_BoomboxMod;
+
+public static class PluginDetails
+{
+    public const string PLUGIN_GUID = "avgduck.plugins.llb.r2mm-boomboxmod";
+    public const string PLUGIN_NAME = "R2MM-BoomboxMod";
+    public const string PLUGIN_VERSION = "1.0.0";
+    
+    public const string DEPENDENCY_LLBMODDINGLIB = "fr.glomzubuk.plugins.llb.llbml";
+    public const string DEPENDENCY_MODMENU = "no.mrgentle.plugins.llb.modmenu";
+
+    public static readonly List<string> MODMENU_TEXT = [
+        
+    ];
+}
